7a9ee1fbc9d6228139b65f5c0c3fbf0786dd0499
Bruce <bahadden@gmail.com>
2018-01-28 14:34:01 +0000

    Tab Ordering sorted into Quarterly Columns

--- docs/problem.md
+++ docs/problem.md
@@ -1,27 +1,70 @@
-A business can be partially exempt from paying VAT. e.g. part of the business is exempt but other parts of the business are not.
+VAT Partial Exemptions
+======================
 
-Where inputs are purchases and supply/output are sales.
+Summary
+-------
 
-There are rules which define whether a Partially Exempt business can claim back 100% of VAT or just the VAT relating to taxable.
+https://www.gov.uk/government/publications/vat-notice-706-partial-exemption/vat-notice-706-partial-exemption
 
-So define:
+Some definitions:
 
-* _De Minimis_: 100% of VAT is reclaimable
-* _Non De Minimis_: VAT related to Taxable Supplies are reclaimable
+_Inputs_ are basically purchases...spending on goods or services by the business. These purchase sometimes include VAT payments, which may be recoverable.
 
-The De Minimis criteria:
+_Output_ or _Supplies_ both refer to _Sales_...a good or service the business sells. These can be taxable or exempt.
 
-1. Exempt inputs are < £625 per month (on average for the quarter/year so £7,500 per year on average)
-2. Exempt inputs are < 50% of total inputs
+_Input Tax_ is therefore the tax (VAT) paid on purchases and _Output Tax_ is the tax paid on sales.
 
-A business that meets both criteria is considered _De Minimis_ and can reclaim all VAT.
+Businesses can generally recover VAT spent of purchases if those purchases related to **taxable** sales. That is, the government doesn't get to tax a product multiple times.  
 
-The De Minimis is generally calculated four times a year:
+A business is _partly exempt_ if it makes, or intends to make, both taxable and exempt supplies and incurs tax on costs which relate to both (that is, parts of the business are exempt but others are not).
 
-* The first three are on quarterly basis (i.e. the previous three months)
-* The final calculation is based on the full year (previous 12 months)
+If a business is partly exempt, it may not be able to recover all input tax. You will have to use a partial exemption method to work out how much input tax can be recovered.
 
-Adjustments can be applied at the _year end_ if any of the previous calculation differ from the final i.e. the year end is the one that counts and the previous calculations were basically estimates.
+_Taxable Input Tax_ is tax paid on Inputs that relate to Taxable **Outputs**. This is normally recoverable. 
+
+Conversely, _Exempt Input Tax_ is tax paid on Inputs that relate to Exempt **Outputs**. These are not normally recoverable.
+
+_Residual Input Tax_ is tax paid on Inputs that relate to both Taxable and Exempt supplies or on general business overheads. Generally only a portion of this is recoverable.
+
+To calculate how much Input Tax the business can recover:
+
+1. Direct attribution of input tax 
+  * Taxable Input Tax - this can be recovered in full
+  * Exempt Input Tax - this can only be recovered if certain criteria are met (see below for de _minimus criteria_)
+2. Apportionment of residual input tax (see below for the _standard method_ of calculating the apportionment)
+
+If the business meets the de minimis criteria (see below) they can also recover the Exempt Input Tax (i.e. they can recover 100% of Input Tax).
+
+### The De Minimis Rules
+
+1. Exempt Input Tax is less than £625 per month (averaged out over the quarter/year); and
+2. Exempt Input Tax is less than 50% of total Input Tax
+
+If both criteria are met, the Exempt Input Tax can be recovered.
+
+### Standard Method for Calculating Apportionment of Residual Tax
+
+We can calculate the percentage of Residual Tax that can be recovered:
+
+    Taxable Portion = Value of Taxable Supplies / Total Values of Supplies x 100
+
+Conversely:
+
+    Exempt Portion = Value of Exempt Supplies / Total Values of Supplies x 100
+	
+Note, these values are net of VAT.
+
+The recovery percentages calculated under the standard method must be rounded up to the next whole number unless you incur more than £400,000 of residual input tax each month on average in which case you must round to two decimal places.
+
+### Quarterly/Yearly Calculations
+
+The rules generally apply for a full year. i.e. a business can only recover all their Input Tax if they meet the de minimus criteria for the full year. 
+
+But they are allowed to calcualate it quarterly and reclaim tax based on the previous three months.
+
+In the fourth quarter, the calculation is applied for the full year.
+
+If it turn out that any of the previous quarterly calculation was 'incorrect' adjustments can be applied to make up the difference.
 
 In order to calculate De Minimis we need to calculate the _Exempt Inputs_, which are calculated as the VAT wholly attributable to _Exempt Supplies__ plus the _Exempt Residual VAT_.
 
@@ -29,8 +72,11 @@ The _Residual_ VAT is the VAT paid on general business expenses that cannot be a
 
 _Total Input_ is all the VAT paid on all types of expenditure: Wholly Attributable + Attributable to Exempt + Residual.
 
-User Inputs
+Calculation
 -----------
+
+### User Inputs
+
 
 In order to calcualate all this we need the user to input:
 
--- src/js/script.js
+++ src/js/script.js
@@ -6,41 +6,50 @@ new Vue({
     data: {
         heading: 'App Heading',
         monthlyAllowance: 625,
+        quarters: ['Quarter 1', 'Quarter 2', 'Quarter 3', 'Quarter 4'],
+        headings: ['Quarter 1', 'Quarter 2', 'Quarter 3', 'Final'],
         state: {
             outputs: {
-                exempt: { Q1: 100, Q2: null, Q3: null, Q4: null },
-                taxable: { Q1: 100, Q2: null, Q3: null, Q4: null }
+                taxable: { Q1: { value: 200.50, tabOrder: 1 }, Q2: { value: null, tabOrder: 6 }, Q3: { value: null, tabOrder: 11 }, Q4: { value: null, tabOrder: 16 } },
+                exempt: { Q1: { value: 100, tabOrder: 2 }, Q2: { value: null, tabOrder: 7 }, Q3: { value: null, tabOrder: 12 }, Q4: { value: null, tabOrder: 17 } }
+                
             },
             inputVAT: {
-                exempt: { Q1: 50, Q2: null, Q3: null, Q4: null },
-                taxable: { Q1: 150, Q2: null, Q3: null, Q4: null },
-                residual: { Q1: 10, Q2: null, Q3: null, Q4: null }
+                taxable: { Q1: { value: 250.55, tabOrder: 3 }, Q2: { value: null, tabOrder: 8 }, Q3: { value: null, tabOrder: 13 }, Q4: { value: null, tabOrder: 18 } },
+                exempt: { Q1: { value: 100, tabOrder: 4 }, Q2: { value: null, tabOrder: 9 }, Q3: { value: null, tabOrder: 14 }, Q4: { value: null, tabOrder: 19 } },
+                residual: { Q1: { value: 10.23, tabOrder: 5 }, Q2: { value: null, tabOrder: 10 }, Q3: { value: null, tabOrder: 15 }, Q4: { value: null, tabOrder: 20 } }
             }
         }
     },
     computed: {
-        percentsExempt: function(){
+        quartersEntered: function() {
+            let outputs = this.state.outputs,
+                inputVAT = this.state.inputVAT,
+                quarter = [];
+
+        },
+        percentsExempt: function() {
             let exempt = this.state.outputs.exempt,
                 taxable = this.state.outputs.taxable,
                 percentsExempt = {};
-            for (var i in exempt){
-                if(exempt[i] > 0 || taxable[i] > 0){
-                    percentsExempt[i] = (exempt[i]/(exempt[i] + taxable[i])*100).toFixed(2);
+            for (var i in exempt) {
+                if (exempt[i].value > 0 || taxable[i].value > 0) {
+                    percentsExempt[i] = (exempt[i].value / (exempt[i].value + taxable[i].value) * 100).toFixed(2);
                 }
                 else {
-                    percentsExempt[i] = null;  
+                    percentsExempt[i] = null;
                 }
             }
             return percentsExempt;
         },
-        totalInputs: function(){
+        totalInputs: function() {
             let exempt = this.state.inputVAT.exempt,
                 taxable = this.state.inputVAT.taxable,
                 residual = this.state.inputVAT.residual,
                 totalInputs = {};
-            for (var i in exempt){
-                if(exempt[i] != null && taxable[i] != null && residual[i] != null){
-                    totalInputs[i] = (exempt[i] + taxable[i] + residual[i]).toFixed(2);
+            for (var i in exempt) {
+                if (exempt[i].value != null && taxable[i].value != null && residual[i].value != null) {
+                    totalInputs[i] = (exempt[i].value + taxable[i].value + residual[i].value).toFixed(2);
                 }
                 else {
                     totalInputs[i] = null;
@@ -48,17 +57,17 @@ new Vue({
             }
             return totalInputs;
         },
-        totalExemptVAT: function(){
+        totalExemptVAT: function() {
             // Exempt Residual VAT = Exempt Supply/Total Supply * Total Residual VAT
             // totalExemptVAT = Exempt Residual VAT + Exempt VAT
             let percentsExempt = this.percentsExempt,
                 residual = this.state.inputVAT.residual,
                 exempt = this.state.inputVAT.exempt,
                 totalExempt = {};
-            for (var i in percentsExempt){
-                if(percentsExempt[i] != null && residual[i] != null){
-                    let erv = (percentsExempt[i]/100 * residual[i]);
-                    totalExempt[i] = (erv + exempt[i]).toFixed(2);
+            for (var i in percentsExempt) {
+                if (percentsExempt[i] != null && residual[i].value != null) {
+                    let erv = (percentsExempt[i] / 100 * residual[i].value);
+                    totalExempt[i] = (erv + exempt[i].value).toFixed(2);
                 }
                 else {
                     totalExempt[i] = null;
@@ -72,8 +81,8 @@ new Vue({
                 exemptInput = this.totalExemptVAT,
                 totalInputs = this.totalInputs,
                 testResults = {};
-            for (var i in exemptInput){
-                if(exemptInput[i] != null && totalInputs[i] != null) {
+            for (var i in exemptInput) {
+                if (exemptInput[i] != null && totalInputs[i] != null) {
                     testResults[i] = {
                         one: exemptInput[i] < quarterlyAllowance,
                         two: exemptInput[i] < (totalInputs[i] / 2)
@@ -90,5 +99,3 @@ new Vue({
         }
     }
 });
-
-  
